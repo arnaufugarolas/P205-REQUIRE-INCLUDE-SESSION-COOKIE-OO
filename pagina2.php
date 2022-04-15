fb51d1168faf33fcb96ea4af284ef3f1f74241f7
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-15 22:24:12 +0200

    Finish pagina2: Show user's history Link header, footer and navbar
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- pagina2.php
+++ pagina2.php
@@ -1,1 +1,17 @@
 <?php
+require('usuari.php');
+
+session_start();
+
+$Title = 'Pagina 2';
+
+require('header.php');
+require('navbar.php');
+require('footer.php');
+echo "<div class='container max-width justify-content-center align-items-center text-center'><H1>".$Title."</H1></div>";
+echo "</br>";
+echo "<div class='container max-width justify-content-center align-items-center text-center'><H3>Usuari: ".htmlspecialchars(($_COOKIE['username'] ?? "no identificat"), ENT_QUOTES, 'UTF-8')."</H3></div>";
+echo "</br>";
+if (isset($_SESSION['username'])) {
+    $_SESSION['username']->printPagines();
+}
