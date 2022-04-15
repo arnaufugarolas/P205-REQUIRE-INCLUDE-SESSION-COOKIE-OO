c5ff441ddb8e59e65a8c00e6a35c42f94359ebf3
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-15 22:20:42 +0200

    Finish index: Create a new user object and save it to session Save username to cookie Link header, footer and navbar
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+require('usuari.php');
+
+session_start();
+
+$Title = 'Index';
+
+if (isset($_GET['username']) && !preg_match('/[^a-zA-Z._-]/', $_GET["username"])) {
+    setcookie('username', $_GET['username'], 0, '/', '', true, true);
+    $_SESSION['username'] = new usuari($_GET['username']);
+    session_write_close();
+    header("Refresh:0; url='index.php'");
+}
+
+require('header.php');
+require('footer.php');
+require('navbar.php');
+
+echo "<div class='container max-width justify-content-center align-items-center text-center'><H1>Hola! ".htmlspecialchars(($_COOKIE['username'] ?? 'Usuari no identificat'), ENT_QUOTES, 'UTF-8')."</H1></div>";
