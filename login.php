3887663285ebb895a18b32dc93dad51f7f9aa15c
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-15 22:21:46 +0200

    Finish login: Send the data of new users to index Link header, footer and navbar
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- login.php
+++ login.php
@@ -1,1 +1,19 @@
 <?php
+require('usuari.php');
+
+session_start();
+
+$Title = 'Login';
+
+require('header.php');
+require('navbar.php');
+require('footer.php');
+?>
+
+<form method="get" action="index.php" name="login">
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Nom:</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nom" name="username" required>
+    </div>
+    <button type="submit" class="btn btn-primary mb-3">Login</button>
+</form>
