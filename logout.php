5018bb0ff9436c340c73f3b89f28b1b856722d77
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-15 22:23:24 +0200

    Finish logout: Destroy the session and expire the user cookie Show user's history Link header, footer and navbar
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- logout.php
+++ logout.php
@@ -1,1 +1,28 @@
 <?php
+require('usuari.php');
+
+session_start();
+
+if (isset($_POST['logout'])) {
+    session_destroy();
+    setcookie('username', '', -1, '/', '', true, true);
+    header("Refresh:0; url='index.php'");
+}
+
+$Title = 'Logout';
+
+require('header.php');
+require('navbar.php');
+require('footer.php');
+?>
+    <br>
+    <div class='container max-width justify-content-center align-items-center text-center'><H3><?php echo "Usuari: ".htmlspecialchars(($_COOKIE['username'] ?? "no identificat"), ENT_QUOTES, 'UTF-8')?></H3></div>
+    <br>
+    <form method="post" action="logout.php" name="logout" class="align-items-center text-center">
+        <input type="hidden" name="logout">
+        <?php if (isset($_COOKIE["username"])) {echo '<button type="submit" class="btn btn-primary mb-3">Logout</button>';} ?>
+    </form>
+<?php
+if (isset($_SESSION['username'])) {
+    $_SESSION['username']->printPagines();
+}
